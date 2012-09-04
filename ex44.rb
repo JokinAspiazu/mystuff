44b54be1232cb3b644a92a4d6eb23d4ff6839c07
Jokin Aspiazu <jokin@peertransfer.com>
2012-09-04 22:47:53 +0200

    forth example

--- ex44.rb
+++ ex44.rb
@@ -1,21 +1,40 @@
-class Parent
+class Parent #creates the Parent class
 
-  def altered ()
+  def override () #defines the override function for the parent class
+    puts "PARENT override ()"
+  end
+  
+  def implicit () #defines the implicit function for the parent class
+    puts "PARENT implicit ()"
+  end
+  
+  def altered () #defines the altered function for the parent class
     puts "PARENT altered ()"
   end
+  
 end
 
-class Child < Parent
+class Child < Parent #defines that child inherits from parent
 
-  def altered()
+  def override() #defines child own override function
+    puts"CHILD override()"
+  end
+
+  def altered() #defines child own override function
     puts "CHILD, BEFORE PARENT altered()"
-    super()
+    super() #and here we call parents three functions
     puts "CHILD, AFTER PARENT altered()"
   end
 end
 
 dad = Parent.new()
 son = Child.new()
+
+dad.implicit()
+son.implicit()
+
+dad.override()
+son.override()
 
 dad.altered()
 son.altered()
