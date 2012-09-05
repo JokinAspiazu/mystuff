9e3080e2555aa73ff66365257c5b99b6655329d0
Jokin Aspiazu <jokin@peertransfer.com>
2012-09-05 23:13:29 +0200

    add project skeleton

---
+++ projects/skeleton/test/test_NAME.rb
@@ -0,0 +1,16 @@
+require 'test/unit'
+
+class MyUnitTests < Test::Unit::TestCase
+
+  def setup
+    puts "setup!"
+  end
+  
+  def teardown
+    puts "teardown!"
+  end
+  
+  def test_basic
+    puts "I RAN!"
+  end
+end
